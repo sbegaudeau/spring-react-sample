3ee62fe5378823789878a62ce075094957dddadb
Stéphane Bégaudeau <stephane.begaudeau@gmail.com>
2020-05-03 11:47:25 +0200

    Add Spring-based backend
    Signed-off-by: Stéphane Bégaudeau <stephane.begaudeau@gmail.com>

---
+++ backend/src/test/java/com/sbegaudeau/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbegaudeau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
